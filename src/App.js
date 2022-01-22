28d5722b0a9698b504719b69541e87dd8a7ee583
Akhil Bhalerao <iamakhil1408@gmail.com>
2022-01-22 22:39:49 +0530

    initial changes and web3 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
 function App() {
+
+  console.log(web3.version)
+  
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './polyfill';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
