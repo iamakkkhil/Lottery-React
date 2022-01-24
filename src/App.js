24d2ae4687a1bda6cd94fb9a6e2429b4718be566
Akhil Bhalerao <iamakhil1408@gmail.com>
2022-01-25 01:40:01 +0530

    code running locally

--- src/App.js
+++ src/App.js
@@ -1,29 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+import lottery from './lottery';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
 
-  console.log(web3.version)
+  // web3.eth.getAccounts().then(console.log);
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: ""
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address)
+    console.log(manager, players, balance)
+    this.setState({ manager, players, balance })
+  }
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts()
+
+    this.setState({ message:"Waiting on transcation success ...." })
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether")
+    })
+
+    this.setState({ message:"You have been entered!" })
+  }
+
+  onClick = async event => {
+    const accounts = await web3.eth.getAccounts()
+
+    this.setState({ message:"Waiting on transcation success ...." })
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    })
+
+    this.setState({ message:"You have been entered!" })
+  }
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+        <p>There are currently {this.state.players.length} people already competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether!</p>
+        <br/>
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter </label>
+            <input
+              input={this.state.value}
+              onChange={event => this.setState({ value:event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+
+        <hr/>
+
+        <h2>Ready to pick a winner?</h2>
+        <button onClick={this.onClick}>Pick a winner !</button>
+
+        <hr/>
+
+        <h2>{this.state.message}</h2>
+
+      </div>
+    )
+  }
 }
 
 export default App;
